63c8013a50b9e41465086a890a38757873994d7a
nageshannamareddy <nagesh.annamareddy@gmail.com>
2022-05-12 12:42:04 +0530

    Added code to generate String violations

---
+++ src/main/java/com/coderewards/codesimulator/StringViolations.java
@@ -0,0 +1,16 @@
+package com.coderewards.codesimulator;
+
+
+/**
+ * Java Class to generate String related Code violations
+ * @author Nagesh A
+ *
+ */
+public class StringViolations {
+	
+	
+	public String convertToUpperCase(String inputString) {
+		return inputString.toUpperCase();
+	}
+
+}
